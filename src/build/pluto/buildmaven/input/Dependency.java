cab46defcc0d92d4a5e3cce22ace1425d3d72836
Sebastian Erdweg <seba.github@gmail.com>
2015-12-02 10:47:02 +0100

    support scoped and optional dependencies

--- src/build/pluto/buildmaven/input/Dependency.java
+++ src/build/pluto/buildmaven/input/Dependency.java
@@ -4,18 +4,22 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.aether.util.artifact.JavaScopes;
+
 public class Dependency implements Serializable {
     private static final long serialVersionUID = -6947544476611313880L;
 
     public final ArtifactConstraint artifactConstraint;
     public final List<Exclusion> exclusions;
     public final long consistencyCheckInterval;
+    public final String scope;
+    public final boolean optional;
 
     /**
      * @param artifactConstraint is the artifactConstraint that the dependency wants to download.
      */
     public Dependency(ArtifactConstraint artifactConstraint) {
-        this(artifactConstraint, new ArrayList<Exclusion>(), 0L);
+        this(artifactConstraint, new ArrayList<Exclusion>());
     }
 
     /**
@@ -38,8 +42,24 @@ public class Dependency implements Serializable {
             ArtifactConstraint artifactConstraint,
             List<Exclusion> exclusions,
             long consistencyCheckInterval) {
+        this(artifactConstraint, exclusions, JavaScopes.COMPILE, false, consistencyCheckInterval);
+    }
+
+    /**
+     * @param artifactConstraint is the artifactConstraint that the dependency wants to download.
+     * @param exclusions are the artifacts that the artifactConstraint depends on but you do
+     * not want to download.
+     */
+    public Dependency(
+            ArtifactConstraint artifactConstraint,
+            List<Exclusion> exclusions,
+            String scope,
+            boolean optional,
+            long consistencyCheckInterval) {
         this.artifactConstraint = artifactConstraint;
         this.exclusions = exclusions;
+        this.scope = scope;
+        this.optional = optional;
         this.consistencyCheckInterval = consistencyCheckInterval;
     }
 
