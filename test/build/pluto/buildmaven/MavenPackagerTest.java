2f054518c4d5cb16f2ca3aa89d93f392dbc93570
Andre Pacak <andre.pacak@googlemail.com>
2015-12-13 12:45:19 +0100

    more meaningfull tests for package builder

--- test/build/pluto/buildmaven/MavenPackagerTest.java
+++ test/build/pluto/buildmaven/MavenPackagerTest.java
@@ -1,47 +1,81 @@
 package build.pluto.buildmaven;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sugarj.common.Exec;
+import org.sugarj.common.Exec.ExecutionResult;
+
 import build.pluto.builder.BuildManagers;
 import build.pluto.builder.BuildRequest;
 import build.pluto.buildmaven.input.MavenPackagerInput;
 import build.pluto.test.build.ScopedBuildTest;
 import build.pluto.test.build.ScopedPath;
 import build.pluto.test.build.TrackingBuildManager;
-
-import org.junit.Test;
-import org.sugarj.common.Exec.ExecutionResult;
-
-import java.io.File;
-
-import static org.junit.Assert.assertEquals;
+import org.sugarj.common.FileCommands;
 
 public class MavenPackagerTest extends ScopedBuildTest {
 	@ScopedPath(value = "")
 	private File rootDir;
 
-	@Test
-	public void testSimplePackage() throws Throwable {
-		MavenPackagerInput input = new MavenPackagerInput
-				.Builder()
-				.setJarName("test")
-				.setWorkingDir(rootDir.getParentFile())
-				.get();
-		ExecutionResult result = pack(input);
+	@ScopedPath(value = "A.java")
+	private File classASource;
+	@ScopedPath(value = "B.java")
+	private File classBSource;
+
+	@Before
+	public void clean() {
+		Exec.run(rootDir, "mvn clean".split(" "));
 	}
 
 	@Test
-	public void testDoubleExecution() throws Throwable {
+	public void testCleanBuild() throws Throwable {
 		MavenPackagerInput input = new MavenPackagerInput
 				.Builder()
 				.setJarName("test")
+				.setSourceDir(rootDir)
 				.setWorkingDir(rootDir)
 				.get();
-		ExecutionResult result = pack(input);
+		assertRun(input);
+	}
 
+	@Test
+	public void testNoRebuild() throws Throwable {
+		MavenPackagerInput input = new MavenPackagerInput
+				.Builder()
+				.setJarName("test")
+				.setSourceDir(rootDir)
+				.setWorkingDir(rootDir)
+				.get();
+		assertRun(input);
 		assertNoRun(input);
 	}
 
-	private ExecutionResult pack(MavenPackagerInput input) throws Throwable {
-		return BuildManagers.build(new BuildRequest<>(MavenPackager.factory, input)).val();
+	@Test
+	public void testRebuildAfterChanges() throws Throwable {
+		MavenPackagerInput input = new MavenPackagerInput
+				.Builder()
+				.setJarName("test")
+				.setSourceDir(rootDir)
+				.setWorkingDir(rootDir)
+				.get();
+		assertRun(input);
+
+		//change source file
+		FileCommands.writeToFile(classASource,
+				"class A { public static void main(String[] args) { System.out.println(\"Altered Class A has run.\"); } }");
+
+		assertRun(input);
+	}
+
+	public void assertRun(MavenPackagerInput input) throws Throwable {
+		TrackingBuildManager m = new TrackingBuildManager();
+		m.require(new BuildRequest<>(MavenPackager.factory, input));
+		assertEquals(1, m.getRequiredInputs().size());
+		assertEquals(1, m.getExecutedInputs().size());
 	}
 
 	public void assertNoRun(MavenPackagerInput input) throws Throwable {
---
+++ testdata/MavenPackagerTest/B.java
@@ -0,0 +1,6 @@
+class B {
+  public static void main(String[] args) {
+    A.main(args);
+    System.out.println("Class B has run.");
+  }
+}
